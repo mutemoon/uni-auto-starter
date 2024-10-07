237818b10d7eb2fd61b0bd12980c7760cf94d9c4
Haoran Zhao <1036066414@qq.com>
2024-10-07 22:08:12 +0800

    更新包版本

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,12 @@
-<template></template>
+<template>
+  <div class="flex justify-center items-center">Hello world!</div>
+</template>
 
 <script setup lang="ts"></script>
 
 <style></style>
+
+<route lang="yaml">
+style:
+  navigationBarTitleText: 首页
+</route>
